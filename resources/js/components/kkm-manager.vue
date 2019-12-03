c367496ab7d3941f83a14719dc0279380b7294b2
sasha.lihih@gmail.com <Cwlziqxy1>
2019-12-03 15:17:29 +0300

    Replays sheetjs to vue-json-excel Added loader for els download

--- resources/js/components/kkm-manager.vue
+++ resources/js/components/kkm-manager.vue
@@ -7,17 +7,15 @@
                         type="is-primary"
                         icon-left="download"
                         :disabled="!checkedDevices.length"
-                        @click="getReport1"
                     >
-                        Сформировать отчёт
-<!--                        <download-excel-->
-<!--                            :fetch="getReport1"-->
-<!--                            :fields = "report1Fields"-->
-<!--                            :before-generate="startDownload"-->
-<!--                            :before-finish="finishDownload"-->
-<!--                        >-->
-<!--                            Сформировать отчёт-->
-<!--                        </download-excel>-->
+                        <download-excel
+                            :fetch="getReport1"
+                            :fields="report1Fields"
+                            :before-generate="startDownload"
+                            :before-finish="finishDownload"
+                        >
+                            Сформировать отчёт
+                        </download-excel>
                     </b-button>
 
 
@@ -42,28 +40,25 @@
 
 <script>
     import kkmManagerDeviceList from './kkm-manager-device-list'
-    import XLSX from 'xlsx';
-    // import JsonExcel from 'vue-json-excel'
+    import JsonExcel from 'vue-json-excel'
 
     export default {
         components: {
             'kkm-manager-device-list': kkmManagerDeviceList,
-            'XLSX': XLSX,
-            // 'download-excel': JsonExcel,
+            'download-excel': JsonExcel,
         },
         data() {
             return {
                 checkedDevices: [],
                 report1: [],
                 loadingComponent: null,
-                // report1Fields: {
-                //     'ID': 'id',
-                //     'Factory number': 'factory_number',
-                //     'Serial number': 'serial_number',
-                //     'Gas station number': 'gas_station_id',
-                //     'Region number': 'region_id',
-                //     'Worked days': 'worked_days'
-                // }
+                report1Fields: {
+                    'ID': 'id',
+                    'Серийный номер': 'serial',
+                    'АЗС': 'azs',
+                    'Регион': 'region',
+                    'Отработано дней': 'worked_days'
+                }
             }
         },
         computed: {
@@ -82,17 +77,13 @@
                 const response = {};
                 try {
                     const response = await axios.post('/reports', {params: params});
-                    this.report1 = response.data;
+                    return response.data;
                 } catch (error) {
                     console.error(error);
                 } finally {
-                    // this.loadingComponent.close()
+                    this.loadingComponent.close()
                 }
 
-                const devices =  XLSX.utils.json_to_sheet(this.report1);
-                const wb = XLSX.utils.book_new();
-                XLSX.utils.book_append_sheet(wb, devices, 'KKM');
-                XLSX.writeFile(wb, 'KKM.xlsx')
             },
             startDownload() {
                 this.loadingComponent = this.$buefy.loading.open()
